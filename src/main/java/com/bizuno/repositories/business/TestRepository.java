4bbb5d2b014af845fff8caf1b1489488d5ba5ebc
AshrafJamadar10 <ashraf_bj.championxi@outlook.com>
2026-09-14 13:57:56 +0530

    removed test

--- src/main/java/com/bizuno/repositories/business/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bizuno.repositories.business;
-
-import com.bizuno.models.business.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
